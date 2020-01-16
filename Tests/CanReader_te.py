e4f04b97fbfe3a4d58d0cecdccf80201d8fd92c0
ondrej.vacek <ondrej.vacek2@tul.cz>
2020-01-16 14:31:41 +0100

    update name of test main

---
+++ Tests/CanReader_te.py
@@ -0,0 +1,20 @@
+"""
+This script will run all defined unit test in chosen directories.
+"""
+
+import subprocess
+
+# List of directory with test files
+directories = ("Communication",
+             "Config",
+             "DataProcessing",
+             "GUI")
+
+if __name__ == "__main__":
+
+    for dir in directories:
+        # Loop over all directory and run test files within
+        cmd = 'python -m unittest discover -s' + dir + ' -p "*_test.py"'
+        print("\n Runing test in /" + dir)
+        subprocess.call(cmd)
+
