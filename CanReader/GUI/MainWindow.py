901309a833546e86dcda64525770221e435efbb4
ondrej.vacek <ondrej.vacek2@tul.cz>
2020-02-10 01:27:47 +0100

    Partial doc update

--- CanReader/GUI/MainWindow.py
+++ CanReader/GUI/MainWindow.py
@@ -6,6 +6,10 @@ from CanReader.GUI.UpdateWindow.UpdateWindow import UpdateWindow
 
 
 class MainWindow(QMainWindow):
+    """
+        Main class of graphical part.
+         Contains all other graphical widget and send data to them
+    """
 
     update_config_signal = QtCore.pyqtSignal()
 
@@ -27,6 +31,11 @@ class MainWindow(QMainWindow):
         self.show()
 
     def update_labels(self, data):
+        """
+            This method takes data received from formula and sends them to place where will be displayed
+            :param data: list of DataPoints containing decoded and processed data from formula
+            :type data: DataPoint
+        """
         value1 = data[0].value
         name1 = data[0].name
         value2 = data[1].value
@@ -38,12 +47,24 @@ class MainWindow(QMainWindow):
         self.label_2_name.setText(name2)
 
     def update_can_msg(self, can_msg):
+        """
+            This method update can message label in gui
+            :param can_msg: last received can message
+        """
         self.can_msg.setText(str(can_msg))
 
     def update_status(self, status):
+        """
+            This method update connection status between app and formula
+            :param status: Status can be "Offline" , "Online" , "Connecting"
+        """
         self.status.setText(status)
 
     def open_update_window(self):
+        """
+            This method is invoked when update button is clicked.
+            Open update window, where user can adjust configuration for selected variable
+        """
         self.update_win = UpdateWindow(self, 1)
 
 
