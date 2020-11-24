0203174444f0d76fb2965ab4bb7bc5fccd2e8903
Formule1 <ondrej.vacek2@tul.cz>
2020-11-24 17:51:41 +0100

    Graph update -> time on x axis & performance increased

--- CanReader/GUI/GraphTabs/Graph.py
+++ CanReader/GUI/GraphTabs/Graph.py
@@ -1,9 +1,27 @@
-from PyQt5 import uic, QtCore
-from PyQt5.QtWidgets import QWidget, QPushButton, QLabel, QAction
-from PyQt5.QtGui import QColor
+import numpy as np
+import datetime
+from PyQt5 import QtCore
 from pyqtgraph import PlotWidget
 import pyqtgraph as pg
-from random import randint
+
+UNIX_EPOCH = datetime.datetime(1970, 1, 1, 0, 0)  # offset-naive datetime
+TS_MULT_us = 1e6
+
+
+def now_timestamp(ts_mult=TS_MULT_us, epoch=UNIX_EPOCH):
+    return int((datetime.datetime.utcnow() - epoch).total_seconds()*ts_mult)
+
+
+def int2dt(ts, ts_mult=TS_MULT_us):
+    return datetime.datetime.utcfromtimestamp(float(ts)/ts_mult)
+
+
+class TimeAxisItem(pg.AxisItem):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def tickStrings(self, values, scale, spacing):
+        return [int2dt(value).strftime("%H:%M:%S") for value in values]
 
 
 class Graph(PlotWidget):
@@ -11,7 +29,7 @@ class Graph(PlotWidget):
     update_signal = QtCore.pyqtSignal(float)
 
     def __init__(self, name, unit, id):
-        PlotWidget.__init__(self)
+        PlotWidget.__init__(self, axisItems={'bottom': TimeAxisItem(orientation='bottom')})
 
         self._name = name
         self._unit = unit
@@ -20,25 +38,71 @@ class Graph(PlotWidget):
         self.update_signal.connect(self.update_data)
         self.setup_graph()
 
-        #Setup data
-        self.x = list(range(3))
-        self.y = [0,0,0]
-        self.pen = pg.mkPen(color=(255, 255, 255))
-        self.data_line = self.plot(self.x, self.y, pen=self.pen)
+        self.view_back = 5 * TS_MULT_us
+        self.vies_front = 1 * TS_MULT_us
 
+        self.data_x = np.empty(1000)
+        self.data_y = np.empty(1000)
+        self.ptr = 0
+
+        self.pen = pg.mkPen(color=(255, 255, 255))
+        self.curve = self.plot(pen=self.pen)
 
     def setup_graph(self):
         self.setBackground((79, 78, 78))
-        self.setTitle(self._name, color='#bdbebf', size='10pt',bold = True)
-        self.setLabel('left', self._unit, color='#bdbebf',  size='15pt')
-        self.setLabel('bottom', 'Sample', color='#bdbebf', size='15pt')
+        self.setClipToView(True)
+        self.setTitle(self._name, color='#bdbebf', size='10pt', bold=True)
+        self.setLabel('left', text=self._unit, color='#bdbebf',  size='15pt')
+        self.setLabel('bottom', text='Time', units='', unitPrefix='', color='#bdbebf', size='15pt')
         self.showGrid(x=True, y=True)
 
+        #self.setMenuEnabled(False)
+
     def update_data(self, value):
-        self.x = self.x[1:]
-        self.x.append(self.x[-1] + 1)
+        if self.ptr >= self.data_x.shape[0]:
+            self.extend_data_storage()
 
-        self.y = self.y[1:]
-        self.y.append(value)
+        time_now = now_timestamp()
+        self.data_x[self.ptr] = time_now
+        self.data_y[self.ptr] = value
 
-        self.data_line.setData(self.x, self.y)  # Update the data.
+        self.curve.setData(x=self.data_x[:self.ptr], y=self.data_y[:self.ptr])
+        self.setXRange(time_now - self.view_back, time_now + self.vies_front)
+        self.ptr += 1
+
+    def extend_data_storage(self):
+        tmp_x = self.data_x
+        self.data_x = np.empty(self.data_x.shape[0] * 2)
+        self.data_x[:tmp_x.shape[0]] = tmp_x
+
+        tmp_y = self.data_y
+        self.data_y = np.empty(self.data_y.shape[0] * 2)
+        self.data_y[:tmp_y.shape[0]] = tmp_y
+
+
+if __name__ == "__main__":
+    from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+    from PyQt5.QtCore import QTimer
+
+    def send_data():
+        value = np.random.normal()
+        graph.update_signal.emit(value)
+
+    app = QApplication([])
+    window = QWidget()
+    layout = QVBoxLayout()
+
+    graph = Graph("Test", "Km/hod", 1)
+
+    layout.addWidget(graph)
+    window.setLayout(layout)
+
+    tmr = QTimer()
+    tmr.timeout.connect(send_data)
+    tmr.start(30)
+    window.show()
+
+    app.exec_()
+
+
+
